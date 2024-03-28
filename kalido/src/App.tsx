2ccf5c44768ce467e7bec7046a5580a51aa27ec6
Zeithrold <zeithrold@gmail.com>
2024-03-28 11:13:56 +0000

    Updated frontend code

---
+++ kalido/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppBarComponent from './components/AppBar'
+
+export default function App (): React.JSX.Element {
+  return (
+    <div>
+      <AppBarComponent />
+    </div>
+  )
+}
---
+++ kalido/src/components/AppBar.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Toolbar from '@mui/material/Toolbar'
+import Done from '@mui/icons-material/Done'
+import Warning from '@mui/icons-material/Warning'
+
+export default function AppBarComponent (): React.JSX.Element {
+  const [connected, setConnected] = React.useState(false)
+  return (
+    <AppBar>
+      <Toolbar variant='dense'>
+        <Typography
+          sx={{
+            flexGrow: 1
+          }}
+          variant="h6"
+        >live2d-pose</Typography>
+        {
+          connected
+            ? <Button
+              variant='contained'
+              color='success'
+              startIcon={<Done />}
+              size='small'
+              onClick={() => {
+                setConnected(false)
+              }}>已连接至 ws://127.0.0.1</Button>
+            : <Button
+              size='small'
+              variant='contained'
+              color='error'
+              startIcon={<Warning />}
+              onClick={() => {
+                setConnected(true)
+              }}>未连接</Button>
+        }
+      </Toolbar>
+    </AppBar>
+  )
+}
